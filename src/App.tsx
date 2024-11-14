f6d6b81d42cab682e1a656b898f73aafb032fd5a
CHO YONGSU <choyongsu2001@naver.com>
2024-11-14 10:19:46 +0900

    라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import {Route , Routes} from "react-router-dom"
 import Login from "./pages/LoginPage/Login";
-
+import SignUp from "./pages/SignUpPage/SignUp";
+import InfoPage from "./pages/SignUpPage/components/UserInfo";
 function App() {
   return (
     <div className="App">
      <Routes>
-      <Route path="/" element={<Login />}/> 
+     <Route path="/" element={<Login />}/> 
+     <Route path="signup" element={<SignUp />}/> 
+     <Route path="userinfo" element={<InfoPage />}/> 
+
      </Routes>
     </div>
   );
